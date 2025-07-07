8e69f0b87599f45d3728131b81b49750ffdada2a
doguto <doguto110@gmail.com>
2025-07-07 20:30:43 +0900

    Feat: 課題1-5の作成

---
+++ 1-5.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <cmath>
+#include <vector>
+#include "matplotlib-cpp/matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
+
+double pareto(double a, double boundary) {
+    double random = (static_cast<double>(rand()) / RAND_MAX);
+    
+    return boundary * pow(1.0 - random, -1.0 / a);
+}
+
+int main() {
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+    const int a = 2;
+    const double boundary = 1;
+    const int N = 2000;
+
+    double sum = 0.0;
+    std::vector<double> y(N + 1, 0.0);
+    for (int i = 1; i <= N; i++) {
+        sum += pow((pareto(a, boundary) - 2), 2);
+        double value = sum / i;
+        y[i] = value;
+    }
+
+    plt::ylabel("Value");
+    plt::xlabel("Count");
+    plt::plot(y);
+    plt::show();
+}
