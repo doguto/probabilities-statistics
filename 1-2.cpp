b973fea25ac55a125cd63ecf6fe83db17094811a
doguto <doguto110@gmail.com>
2025-07-07 14:48:44 +0900

    Feat: 1-2の課題の作成

--- 1-2.cpp
+++ 1-2.cpp
@@ -1,19 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
-#include <matplotlibcpp.h>
+#include <vector>
+#include "matplotlib-cpp/matplotlibcpp.h"
+
+namespace plt = matplotlibcpp;
 
 double exponential(double lambda) {
     double random = static_cast<double>(rand()) / RAND_MAX;
-    return lambda * exp(-lambda * random);
+    return -std::log(1.0 - random) / lambda;
 }
 
 int main() {
     const int lambda = 2;
     const int N = 20000;
 
-    double exponentialValues[N];
+    std::vector<double> distributions(101, 0);
     for (int i = 0; i < N; ++i) {
-        exponentialValues[i] = exponential(lambda);
+        double value = exponential(lambda);
+        int index = static_cast<int>(value / 0.1);
+        distributions[std::min(index, 100)]++;
     }
+
+    plt::ylabel("Count");
+    plt::xlabel("Random Value [x0.1]");
+    plt::bar(distributions);
+    plt::show();
 }
